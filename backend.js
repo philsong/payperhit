be83a19403a1bdc885d5398d58761cd1c199d04d
Esteban Ordano <eordano@gmail.com>
2015-01-18 15:12:24 -0300

    filling in the blanks

---
+++ backend.js
@@ -0,0 +1,136 @@
+var bitcore = require('bitcore');
+var async = require('async');
+var leveldb = require('level');
+
+function LevelUpBackend(opts) {
+  this.db = leveldb(opts);
+}
+
+var PUBKEY_TO_PRIVKEY = 'pubkey-';
+var PUBKEY_TO_PAYMENT_PRIVATE_KEY  = 'paymentkey-';
+var PUBKEY_TO_PAYMENT = 'transaction-';
+var PUBKEY_TO_BALANCE = 'balance-';
+
+LevelUpBackend.Errors = {
+  DBERROR: 'Unexpected Error when operating with database'
+};
+
+LevelUpBackend.prototype.createPrivateKey = function(callback) {
+  var privateKey = new bitcore.PrivateKey();
+  var key = PUBKEY_TO_PRIVKEY + privateKey.publicKey.toString();
+  this.db.put(key, privateKey.toString(), function(err) {
+    if (err) {
+      console.log(err);
+      return callback(LevelUpBackend.Errors.DBERROR);
+    }
+    return callback(null, privateKey);
+  });
+};
+
+LevelUpBackend.prototype.getPrivateKey = function(publicKey, callback) {
+  var key = PUBKEY_TO_PRIVKEY + publicKey;
+  this.db.get(key, function(err, result) {
+    if (err) {
+      console.log(err);
+      return callback(LevelUpBackend.Errors.DBERROR);
+    }
+    return callback(null, new bitcore.PrivateKey(result));
+  });
+};
+
+LevelUpBackend.prototype.createPaymentAddressFor = function(publicKey, callback) {
+  var key = PUBKEY_TO_PAYMENT_PRIVATE_KEY + publicKey;
+  var privateKey = new bitcore.PrivateKey();
+  var address = privateKey.toAddress();
+  this.db.put(key, privateKey.toString(), function(err) {
+    if (err) {
+      console.log(err);
+      return callback(err);
+    }
+    return callback(null, address);
+  });
+};
+
+LevelUpBackend.prototype.getPaymentKeyFor = function(publicKey, callback) {
+  var key = PUBKEY_TO_PAYMENT_PRIVATE_KEY + publicKey;
+  this.db.get(key, function(err, data) {
+    if (err) {
+      console.log(err);
+      return callback(err);
+    }
+    var privateKey = new bitcore.PrivateKey(data);
+    return callback(null, privateKey);
+  });
+};
+
+LevelUpBackend.prototype.getLastPaymentTransaction = function(publicKey, callback) {
+  var key = PUBKEY_TO_PAYMENT + publicKey;
+  this.db.get(key, function(err, data) {
+    if (err) {
+      console.log(err);
+      return callback(err);
+    }
+    return callback(err, new channel.Transaction.Payment(data));
+  });
+};
+
+LevelUpBackend.prototype.savePaymentTransaction = function(publicKey, transaction, callback) {
+  var key = PUBKEY_TO_PAYMENT + publicKey;
+  this.db.put(key, transaction.toJSON(), function(err) {
+    if (err) {
+      console.log(err);
+      return callback(err);
+    }
+    return callback();
+  });
+};
+
+LevelUpBackend.prototype.getUsedBalance = function(publicKey, callback) {
+  var key = PUBKEY_TO_BALANCE + publicKey;
+  this.db.get(key, function(err, data) {
+    if (err) {
+      console.log(err);
+      return callback(err);
+    }
+    return callback(err, data);
+  });
+};
+
+LevelUpBackend.prototype.saveUsedBalance = function(publicKey, balance, callback) {
+  var key = PUBKEY_TO_BALANCE + publicKey;
+  this.db.put(key, balance, function(err) {
+    if (err) {
+      console.log(err);
+      return callback(err);
+    }
+    return callback();
+  });
+};
+
+LevelUpBackend.prototype.getProvider = function(publicKey, paymentAddress, callback) {
+  async.parallel([
+    function(callback) {
+      getPrivateKey(publicKey, callback);
+    },
+    function(callback) {
+      getPaymentKeyFor(publicKey, function(err, paymentKey) {
+        // Ignore errors (there may not be any payment key)
+        return callback(null, paymentKey);
+      });
+    },
+    function(callback) {
+      getLastPaymentTransaction(publicKey, callback);
+    }
+  ], function(err, results) {
+    if (err) {
+      return callback(err);
+    }
+    var provider = new channel.Provider({
+      key: results[0],
+      paymentAddress: results[1] ? results[1].toAddress() : paymentAddress,
+      currentAmount: results[2].currentAmount
+    });
+    provider.paymentTx = results[2];
+    return callback(null, provider);
+  });
+};
--- middleware.js
+++ middleware.js
@@ -1,12 +1,11 @@
+var bitcore = require('bitcore');
+var bodyParser = require('body-parser');
 
 var $ = function(check, message) {
   if (!check) {
     throw new Error('Invalid state: ' + message);
   }
 };
-
-function levelup(opts) {
-}
 
 function payperhit(opts) {
   var self = this;
@@ -20,18 +19,64 @@ function payperhit(opts) {
   this.backend = opts.backend;
 
   /**
+   * @type {bitcore.Address}
+   * @desc The address where all payments will go. If set to null, a private key
+   * will be created for each refund signed
+   */
+  this.paymentAddress = new bitcore.Address(opts.paymentAddress);
+
+  /**
    * Parses headers and retrieves information about any payment updates in the
    * headers of the request
    */
+  var PUBLIC_KEY_HEADER = 'X-PPH-PUBLIC-KEY';
+  var PAYMENT_UPDATE = 'X-PPH-PAYMENT';
   var middleware = function(req, res, next) {
+    var setUsedBalance = function() {
+      backend.getUsedBalance(req.headers.get(PUBLIC_KEY_HEADER), function(err, balance) {
+        if (err) {
+          return res.end('Internal error when retrieving payment channel status');
+        }
+        req.pph.usedBalance = balance;
+        next();
+      });
+    };
+    req.pph = req.pph || {};
+    if (!req.headers.get(PUBLIC_KEY_HEADER)) {
+      return res.end('Must specify the payment channel public key with ' + PUBLIC_KEY_HEADER);
+    }
+    backend.getProvider(req.headers.get(PUBLIC_KEY_HEADER), function(err, provider) {
+      req.pph.provider = provider;
+      req.pph.currentAmount = provider.currentAmount;
+      if (req.headers.get(PAYMENT_UPDATE)) {
+        try {
+          provider.validatePayment(JSON.parse(req.headers.get(PAYMENT_UPDATE)));
+          backend.updateLastPayment(publicKey, provider.currentAmount, function(err) {
+            if (err) {
+              console.log(err);
+              return res.end('Internal error');
+            }
+            req.pph.currentAmount = provider.currentAmount;
+            setUsedBalance();
+          });
+        } catch (error) {
+          console.log(error);
+          return res.end('Invalid payment received');
+        }
+      } else {
+        setUsedBalance();
+      }
+    });
   };
+
+  middleware.requests = {};
 
   /**
    * Default route: "/_pph/init"
    * Returns a public key so a payment channel can be initiated
    */
-  middleware.init = function(req, res) {
-    self.backend.createPrivKey(function(err, privateKey) {
+  middleware.requests.init = function(req, res) {
+    self.backend.createPrivateKey(function(err, privateKey) {
       response.end(privateKey.publicKey.toString());
     });
   };
@@ -40,28 +85,160 @@ function payperhit(opts) {
    * Default route: "/_pph/setup"
    * Receives a public key and a refund transaction to be signed.
    */
-  middleware.setup = function(callback) {
+  middleware.requests.setup = function(req, res) {
+    if (!req.body) {
+      return res.end('Must provide a json body for this request');
+    }
+
+    var publicKey = req.body.publicKey;
+    var refund = req.body.refund;
+
+    if (!publicKey) {
+      return res.end('Must provide a `publicKey` param in the json body');
+    }
+    if (!refund) {
+      return res.end('Must provide a `refund` param in the json body');
+    }
+
+    backend.getPrivateKey(publicKey, function(err, privateKey) {
+      if (err) {
+        console.log(err);
+        return res.end('Unable to retrieve information about that public key');
+      }
+
+      var provider = channel.Provider({
+        key: privateKey
+      });
+
+      var response = {};
+      try {
+        response.refund = provider.signRefund(refund).toJSON();
+      } catch (error) {
+        console.log(error);
+        return res.end('Internal error on refund signing');
+      }
+
+      if (self.paymentAddress) {
+        response.paymentAddress = self.paymentAddress;
+        return res.end(response);
+      } else {
+        backend.createPaymentAddressFor(publicKey, function(err, paymentAddress) {
+          if (error) {
+            console.log(error);
+            return res.end('Internal error on refund signing');
+          }
+          response.paymentAddress = paymentAddress;
+          return res.end(response);
+        });
+      }
+    });
   };
 
   /**
    * Default route: "/_pph/start"
-   * Receives a commitment transaction
+   * Receives a first payment transaction
    */
-  middleware.start = function(callback) {
+  middleware.requests.start = function(callback) {
+    if (!req.body) {
+      return res.end('Must provide a json body for this request');
+    }
+
+    var publicKey = req.body.publicKey;
+    var paymentTx = req.body.payment;
+
+    if (!publicKey) {
+      return res.end('Must provide a `publicKey` param in the json body');
+    }
+    if (!paymentTx) {
+      return res.end('Must provide a `payment` param in the json body');
+    }
+    var provider = channel.Provider({});
+    async.waterfall([
+      function(callback) {
+        backend.getPrivateKey(publicKey, function(err, privateKey) {
+          if (err) {
+            console.log(err);
+            return callback('Unable to retrieve information about that public key');
+          }
+          provider.key = privateKey;
+          return callback();
+        });
+      },
+      function(callback) {
+        if (self.paymentAddress) {
+          provider.paymentAddress = self.paymentAddress;
+          return callback();
+        } else {
+          backend.getPaymentKeyFor(publicKey, function(err, paymentKey) {
+            if (err) {
+              console.log(err);
+              return callback(err);
+            }
+            provider.paymentAddress = paymentKey.toAddress();
+            return callback();
+          });
+        }
+      },
+      function(callback) {
+        try {
+          provider.validPayment(paymentTx);
+          return backend.savePaymentTransaction(provider.paymentTx, callback);
+        } catch (e) {
+          console.log(e);
+          return callback('Invalid payment');
+        }
+      }
+    ], function(err) {
+      if (err) {
+        return res.end('Bad request or internal error');
+      }
+      return res.end(JSON.stringify({success: true}));
+    });
   };
 
   /**
    * Default route: "/_pph/end"
    * Closes up the payment channel
    */
-  middleware.end = function(callback) {
+  middleware.requests.end = function(callback) {
+    // TODO
   };
 
   /**
    * Decrements the balance for the payment channel, fails if not enough funds.
    */
-  middleware.charge = function(req, res, next) {
+  middleware.charge = function(amount) {
+    return function(req, res, next) {
+      middleware(req, res, function(req, res) {
+        var available = req.pph.currentAmount - req.pph.usedBalance;
+        if (available < amount) {
+          res.statusCode(402);
+          return res.end('Insufficient funds: available ' + available + ', need ' + amount);
+        }
+        next();
+      });
+    };
+  };
+
+  middleware.setup = function(app) {
+    var jsonParser = bodyParser.json();
+
+    app.use(middleware);
+
+    // Setup the endpoint that returns a new public key ("session id?")
+    app.post('/_pph/init', middleware.requests.init);
+
+    // Sign the refund transaction
+    app.post('/_pph/setup', jsonParser, middleware.requests.setup);
+
+    // Takes the commitment transaction and first payment
+    app.post('/_pph/start', jsonParser, middleware.requests.start);
+
+    // Finishes the payment channel cleanly
+    app.post('/_pph/end', jsonParser, middleware.requests.end);
   };
 
   return middleware;
 }
+
+module.exports = payperhit;
